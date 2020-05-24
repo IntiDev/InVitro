416c2d9a35fee6ed0a175becdd255daccf5335cf
IntiDev <yeseniase@outlook.it>
2020-05-23 20:53:07 -0500

    [ADD] Basic footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Header user="Administrador" userName="Fulanito González" />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae placeat
+      fuga, velit, numquam, praesentium consequuntur quos magnam non aliquam rem
+      quisquam enim aliquid autem corrupti porro ipsam exercitationem pariatur
+      tenetur!
+      <Footer clinicalName="In Vitro" />
     </div>
   );
 };
