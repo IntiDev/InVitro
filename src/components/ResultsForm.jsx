8e2718f72e22797df826978c6de25cfadb9d06f9
Luis Caraballo <luisred61@gmail.com>
2020-05-24 13:54:29 -0500

    [ADD] Create basic form to upload exams results

---
+++ src/components/ResultsForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../styles/components/ResultsForm.styl';
+
+const RegisterForm = props => {
+  const { user, id, exam, typeId, examResult } = props;
+  return (
+    <div className="resultsCard">
+      <div className="resultsCard__userInfo">
+        <h1>{user}</h1>
+        <h2>
+          {typeId} 
+          {' '}
+          {id}
+        </h2>
+        <h2>{exam}</h2>
+      </div>
+      <form action="" className="resultsCard__form">
+        {examResult.map(result => (
+          <label htmlFor={result.name} key={result.name}>
+            {result.name}
+            <input type="text" />
+          </label>
+        ))}
+      </form>
+      <button className="resultsCard__submitBtn" type="button">
+        {' '}
+        Cargar Resultados
+      </button>
+    </div>
+  );
+};
+
+export default RegisterForm;
