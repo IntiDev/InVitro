a2e2756a52b039af08634ecc8501a2953ac496ff
Luis Caraballo <luisred61@gmail.com>
2020-05-24 00:02:39 -0500

    [REMOVE] Delete initialState.json and change background Color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import fetch from 'node-fetch';
+import React from 'react';
 import '../styles/components/App.styl';
 import ExamsModal from './ExamModal';
 
 const App = () => {
-  const [info, setInfo] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/exams')
-      .then(response => response.json())
-      .then(data => setInfo(data));
-  }, []);
-
+  const info = {
+    exams: [
+      {
+        id: 1,
+        name: 'Examen de Sangre',
+      },
+      {
+        id: 2,
+        name: 'Examen de Orina',
+      },
+      {
+        id: 3,
+        name: 'Examen de Otra cosa',
+      },
+    ],
+  };
   return (
     <div className="App">
-      <ExamsModal exams={info} />
+      <ExamsModal exams={info.exams} />
     </div>
   );
 };
