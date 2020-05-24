2e8d8db3fd9a51b54e60736619255d5f38df8ae2
Luis Caraballo <luisred61@gmail.com>
2020-05-23 23:29:03 -0500

    [ADD] Apply basic css to the form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import RegisterForm from './RegisterForm';
 const App = () => {
   return (
     <div className="App">
-      <RegisterForm />
+      <RegisterForm user="medico" />
     </div>
   );
 };
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,26 +3,52 @@
 import React from 'react';
 import '../styles/components/RegisterForm.styl';
 
-const RegisterForm = () => {
+const RegisterForm = props => {
+  const { user } = props;
+
   return (
     <div className="registerCard">
-      <h1 className="registerCard__title">Datos del Medico</h1>
+      <h1 className="registerCard__title">
+        Datos del
+        {user}
+      </h1>
       <form className="registerCard__form" action="">
-        <label htmlFor="typeID">Tipo de Documento</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Documento</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Nombre</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Apellido</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Celular</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Tlf Fijo</label>
-        <input type="text" name="typeID" id="typeID" />
-        <label htmlFor="typeID">Correo</label>
-        <input type="text" name="typeID" id="typeID" />
-        <input type="submit" />
+        <label htmlFor="typeID">
+          Tipo de Documento
+          <input type="text" name="typeID" id="typeID" />
+        </label>
+
+        <label htmlFor="document">
+          Documento
+          <input type="text" name="document" id="document" />
+        </label>
+
+        <label htmlFor="name">
+          Nombre
+          <input type="text" name="name" id="name" />
+        </label>
+
+        <label htmlFor="lastname">
+          Apellido
+          <input type="text" name="lastname" id="lastname" />
+        </label>
+
+        <label htmlFor="celphone">
+          Celular
+          <input type="text" name="celphone" id="celphone" />
+        </label>
+
+        <label htmlFor="phoneNumber">
+          Tlf Fijo
+          <input type="text" name="phoneNumber" id="phoneNumber" />
+        </label>
+
+        <label htmlFor="mail">
+          Correo
+          <input type="mail" name="mail" id="mail" />
+        </label>
+
+        <input type="submit" className="registerCard__submitButtom" />
       </form>
     </div>
   );
