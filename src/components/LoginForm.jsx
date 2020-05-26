56df6be72883aa645db1a8e83ec2427ce9b2edc2
IntiDev <yeseniase@outlook.it>
2020-05-25 22:28:15 -0500

    [UPDATE] scaffolding

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from './Button';
+import '../styles/components/LoginForm.styl';
+
+const LoginForm = props => {
+  const { title } = props;
+  const clickMe = () => {
+    console.log('Click!');
+  };
+
+  return (
+    <div className="Login">
+      <p className="Login__title">{title}</p>
+      <input
+        type="email"
+        className="Login__input"
+        placeholder="usuario@correo.com"
+      />
+      <input type="password" className="Login__input" placeholder="********" />
+      <Button text="Login" onClick={clickMe} />
+    </div>
+  );
+};
+
+export default LoginForm;
