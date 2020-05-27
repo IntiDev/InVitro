51c3df7b821c6f5a7c2783de793f67cfbffa3833
IntiDev <yeseniase@outlook.it>
2020-05-27 02:03:25 -0500

    [ADD] login view

---
+++ src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+import '../styles/pages/Login.styl';
+
+const Login = () => {
+  return (
+    <div className="Login">
+      <section className="Login__info">
+        <img
+          className="Login__logo"
+          src="http://resource.intidev.info/InVitro/logo.png"
+          alt="Logo"
+        />
+        <div className="Login__text">
+          <p>
+            &quot;La salud es el regalo más grande, cuídala y protégete&quot;
+          </p>
+          <p>
+            Comprometidos con
+            <br /> <strong> tus estudios</strong>
+          </p>
+        </div>
+      </section>
+      <section className="Login__form">
+        <LoginForm title="Bienvenido" />
+      </section>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from '../components/Header';
 import Layout from '../components/Layout';
+import Login from '../pages/Login';
 
 const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        {/* It's just an test */}
-        <Route exact path="/Header" component={Header} />
+        <Route exact path="/" component={Login} />
       </Switch>
     </Layout>
   </BrowserRouter>
