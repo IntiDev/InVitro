172e87f1be4965ebd557ac26c2a4a22acc1cdb11
Luis Caraballo <luisred61@gmail.com>
2020-05-26 19:25:23 -0500

    [ADD] Inserts an action bar to the table in AdminUserHome page

--- src/pages/AdminUserHome.jsx
+++ src/pages/AdminUserHome.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import SideMenu from '../components/SideMenu';
 import Table from '../components/Table';
 import '../styles/pages/AdminUserHome.styl';
+import Button from '../components/Button';
 
 const AdminUserHome = () => {
   return (
     <div className="userHome__container">
       <SideMenu />
-      <Table />
+      <div className="userHome__table">
+        <div className="userHome__tableActions">
+          <div className="userHome__tableActions__filter">
+            <h2> Titulo Tabla</h2>
+            <input type="text" placeholder="Filter" />
+          </div>
+          <div>
+            <Button text="Nuevo Usuario" />
+            <Button text="Cargar CSV " />
+          </div>
+        </div>
+        <Table />
+      </div>
     </div>
   );
 };
