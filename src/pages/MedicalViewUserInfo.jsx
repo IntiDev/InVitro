57bc1e74b37f112b22f550b16c535493d4df3673
IntiDev <yeseniase@outlook.it>
2020-05-30 04:20:27 -0500

    [ADD] base to MedicalViewUserInfo page

---
+++ src/pages/MedicalViewUserInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import SideMenu from '../components/SideMenu';
+// import ResultsForm from '../components/ResultsForm';
+import '../styles/pages/MedicalViewUSerInfo.styl';
+
+const MedicalViewUSerInfo = () => {
+  return <>{/* <SideMenu/>
+    <ResultsForm/> */}</>;
+};
+
+export default MedicalViewUSerInfo;
