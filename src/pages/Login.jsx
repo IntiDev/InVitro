d3382022bc1b9cc14750a3a17946782ad82d8574
IntiDev <yeseniase@outlook.it>
2020-05-30 03:34:59 -0500

    [UPDATE] several basic components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import Footer from '../components/Footer';
 import '../styles/pages/Login.styl';
 
 const Login = () => {
   return (
-    <div className="Login">
-      <section className="Login__info">
-        <img
-          className="Login__logo"
-          src="http://resource.intidev.info/InVitro/logo.png"
-          alt="Logo"
-        />
-        <div className="Login__text">
-          <p>
-            &quot;La salud es el regalo más grande, cuídala y protégete&quot;
-          </p>
-          <p>
-            Comprometidos con
-            <br /> <strong> tus estudios</strong>
-          </p>
-        </div>
-      </section>
-      <section className="Login__form">
-        <LoginForm title="Bienvenido" />
-      </section>
-    </div>
+    <>
+      <div className="Login">
+        <section className="Login__info">
+          <img
+            className="Login__logo"
+            src="http://resource.intidev.info/InVitro/logo.png"
+            alt="Logo"
+          />
+          <div className="Login__text">
+            <p>
+              &quot;La salud es el regalo más grande, cuídala y protégete&quot;
+            </p>
+            <p>
+              Comprometidos con
+              <br /> <strong> tus estudios</strong>
+            </p>
+          </div>
+        </section>
+        <section className="Login__form">
+          <LoginForm title="Bienvenido" />
+        </section>
+      </div>
+      <Footer clinicalName=" In Vitro" />
+    </>
   );
 };
 
