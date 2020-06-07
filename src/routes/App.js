f4980c6a590eb4982d7bf7d20c2069a047308971
IntiDev <yeseniase@outlook.it>
2020-06-07 00:49:44 -0500

    [ADD] patient base view

--- src/routes/App.js
+++ src/routes/App.js
@@ -7,6 +7,8 @@ import Login from '../pages/Login';
 import MedicalSearchView from '../pages/MedicalSearchView';
 import MedicalViewUSerInfo from '../pages/MedicalViewUserInfo';
 import MedicalUserExams from '../pages/MedicalUserExams';
+import PatientInfo from '../pages/PatientInfo';
+import PatientExams from '../pages/PatientExams';
 
 const App = () => (
   <BrowserRouter>
@@ -18,6 +20,8 @@ const App = () => (
         <Route exact path="/medical" component={MedicalSearchView} />
         <Route exact path="/medicalUserInfo" component={MedicalViewUSerInfo} />
         <Route exact path="/medicalUserExams" component={MedicalUserExams} />
+        <Route exact path="/patientInfo" component={PatientInfo} />
+        <Route exact path="/patientExams" component={PatientExams} />
       </Layout>
     </Switch>
   </BrowserRouter>
